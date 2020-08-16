a97576a4bfcf6482b56ab9856b093bd4594b904c
Brian <wolkomirski.brian@gmail.com>
2020-08-16 18:46:05 -0300

    Firebase Sync

--- app/src/main/java/com/wolkorp/petrescue/MainActivity.kt
+++ app/src/main/java/com/wolkorp/petrescue/MainActivity.kt
@@ -2,9 +2,15 @@ package com.wolkorp.petrescue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
