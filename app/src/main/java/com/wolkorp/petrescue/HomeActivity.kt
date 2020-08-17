fa27d40625bc0b73abbcad3db9df0edb3863719f
Brian <wolkomirski.brian@gmail.com>
2020-08-16 22:39:53 -0300

    backend Login BASIC

---
+++ app/src/main/java/com/wolkorp/petrescue/HomeActivity.kt
@@ -0,0 +1,35 @@
+package com.wolkorp.petrescue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType{
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        //setup
+        val bundle: Bundle?= intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?:"", provider ?:"")
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Inicio"
+        emailTextView.text= provider
+        providerTextView.text = email
+
+        logOutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
